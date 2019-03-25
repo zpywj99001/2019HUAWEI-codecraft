d6768a93bedd590bcefc2373189089c5ba659cb9
V.J <670744484@qq.com>
2019-03-25 23:56:58 +0800

    Update _FindShortPath.py

--- program/_FindShortPath.py
+++ program/_FindShortPath.py
@@ -70,7 +70,9 @@ class _FindShortPath(object):     # 寻找最优路径类
         for road in originCross.roadList:          # 和起始路口直接相连的路口，则修改相应路长，并记录下路径
             if road != -1:
                 path = _Path()
-                if road.endID == startCrossID:
+                if road.endID == startCrossID and 0 == road.isTwoWay:   # 判断当前道路方向是否可以通行
+                    continue
+                elif road.endID == startCrossID and 1 == road.isTwoWay:
                     road.endID, road.startID = road.startID, road.endID 
                 path.currentCrossID = road.endID
                 path.length = road.length
@@ -99,20 +101,22 @@ class _FindShortPath(object):     # 寻找最优路径类
             
     def FindShortPath(self, map1):                 # 找出起始点至终点的最短路径
         nextCross = self.FindNextCross(map1)
-        print('nextCross.ID:',nextCross.ID)
+#        print('nextCross.ID:',nextCross.ID)
         while nextCross.ID:                                      # 若该路口能使路径长度变小，则将该路口存进路口ID列表，直到所有路口都处理完毕
             currentPath = self.pathDic[nextCross.ID]
             
             for road in nextCross.roadList:
                 if road != -1:
-                    if road.endID == nextCross.ID:
+                    if road.endID == nextCross.ID and 0 == road.isTwoWay:    # 判断当前道路方向是否可以通行
+                        continue
+                    elif road.endID == nextCross.ID and 1 == road.isTwoWay:
                         road.endID, road.startID = road.startID, road.endID
                     if road.endID in currentPath.pathCrossList:
                         continue
                     targetPath = self.pathDic[road.endID]
                     tempLength = currentPath.length + road.length
-                    print('targetPath.length:', targetPath.length)
-                    print('tempLength:', tempLength)
+#                    print('targetPath.length:', targetPath.length)
+#                    print('tempLength:', tempLength)
                     if tempLength < targetPath.length:
                         targetPath.length = tempLength
                         targetPath.pathCrossList = []
@@ -124,7 +128,7 @@ class _FindShortPath(object):     # 寻找最优路径类
                     
             self.pathDic[nextCross.ID].isProcessed = True    # 标记为已处理          
             nextCross = self.FindNextCross(map1)          # 继续寻找下一个路口
-            print('nextCross.ID:',nextCross.ID)
+#            print('nextCross.ID:',nextCross.ID)
                 
         
 
