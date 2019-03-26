cdedc4ffd997f0e86b9cddbe059cf85a498c1283
Suxve <32962409+Suxve@users.noreply.github.com>
2019-03-26 12:28:25 +0800

    add save answer.txt Module

--- program/init.py
+++ program/init.py
@@ -8,12 +8,11 @@ Created on Thu Mar 21 19:37:50 2019
 from _FindShortPath import _Cross, _Road, _Channel, _Path, _Map, _FindShortPath
 from _Car import _Car
 
-
 def ReadCartxt(folder, file):                                                  # 读取车辆信息
     fileDict = {file+'Id': file+'_attr_list'}
     fileIdOrder = []
-    txt = open(folder+"\\"+file+".txt", "r")
-    lines = txt.readlines()[1:]
+    with open(folder+"/"+file+".txt", "r") as f:
+        lines = f.readlines()[1:]
     for line in lines:
         line = line.strip('( )\n')
         line = line.split(',')
@@ -28,8 +27,8 @@ def ReadCartxt(folder, file):                                                  #
 def ReadCrosstxt(folder, file):                                                # 读取路口信息
     fileDict = {file+'Id': file+'_attr_list'}
     fileIdOrder = []
-    txt = open(folder+"\\"+file+".txt", "r")
-    lines = txt.readlines()[1:]
+    with open(folder+"/"+file+".txt", "r") as f:
+        lines = f.readlines()[1:]
     for line in lines:
         line = line.strip('( )\n')
         line = line.split(',')
@@ -46,28 +45,41 @@ def ReadCrosstxt(folder, file):                                                #
 def ReadRoadtxt(folder, file):                                                 # 读取道路信息
     fileDict = {file+'Id': file+'_attr_list'}
     fileIdOrder = []
-    txt = open(folder+"\\"+file+".txt", "r")
-    lines = txt.readlines()[1:]
-    for line in lines:
-        line = line.strip('( )\n')
-        line = line.split(',')
-        for i in range(len(line)):
-            line[i] = int(line[i])
-        newRoad = _Road()
-        newRoad.ID = line[0]
-        newRoad.length = line[1]
-        newRoad.limitSpeed = line[2]
-        newRoad.channelNum = line[3]
-        newRoad.startID = line[4]
-        newRoad.endID = line[5]
-        newRoad.isTwoWay = line[6]
-        newRoad.channelList = list(range(1,newRoad.channelNum+1))
-        fileDict[newRoad.ID] = newRoad
-        fileIdOrder.append(line[0])
-        fileIdOrder.sort()
+    with open(folder+"/"+file+".txt", "r") as f:
+        lines = f.readlines()[1:]
+        for line in lines:
+            line = line.strip('( )\n')
+            line = line.split(',')
+            for i in range(len(line)):
+                line[i] = int(line[i])
+            newRoad = _Road()
+            newRoad.ID = line[0]
+            newRoad.length = line[1]
+            newRoad.limitSpeed = line[2]
+            newRoad.channelNum = line[3]
+            newRoad.startID = line[4]
+            newRoad.endID = line[5]
+            newRoad.isTwoWay = line[6]
+            newRoad.channelList = list(range(1, newRoad.channelNum+1))
+            fileDict[newRoad.ID] = newRoad
+            fileIdOrder.append(line[0])
+            fileIdOrder.sort()
     return fileDict, fileIdOrder
 
-fileDir = r"G:\VJ\华为软挑\2019华为软件精英挑战赛\2019软挑-初赛-SDK\SDK_python\CodeCraft-2019\config"
+def _SaveAnswertoTxt(filePath):
+    answerLine = []
+    answerLine.append("#(carId, StartTime, RoadIdList)")
+    for carId in carIdOrder:
+        optPathStr = ", ".join(str(i) for i in optPath[carId])
+        optPathStr = "(" + str(car)+", " + str(carDict[carId].startTime) + optPathStr + ")"
+        answerLine.append(optPathStr)
+    answerTxt = "\n".join(answerLine)
+    with open(filePath, "w") as f:
+        f.write(answerTxt)
+        print("Save answer.txt successfully!\n")
+
+
+fileDir = "../config"            #这里不建议加上r，写answer.txt文件时也需要用路径,暂时去掉
 
 crossDict, crossIdOrder = ReadCrosstxt(fileDir, "cross")
 roadDict, roadIdOrder = ReadRoadtxt(fileDir, "road")
@@ -113,6 +125,7 @@ for car in carIdOrder:
     nowOptPath.append(nowCar.end)
     optPath[car] = nowOptPath
 
+_SaveAnswertoTxt(fileDir + "/answer.txt")
 
 
 
