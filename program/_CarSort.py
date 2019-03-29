63086d04e87b11513bbc2bf72c7f17898a7025ce
V.J <670744484@qq.com>
2019-03-29 23:57:35 +0800

    Update _CarSort.py

--- program/_CarSort.py
+++ program/_CarSort.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Mar 28 19:11:24 2019
-
 @author: just4
 """
 
-
-def CarSorted(fold,StartTime):
+def CarSorted(fold):
     """
     Description:
     :fold:input,the answer.txt which the departing cars have been removed
@@ -21,12 +19,7 @@ def CarSorted(fold,StartTime):
             line = line.strip('( )\n').split(',')
             for i in range(len(line)):
                 line[i] = int(line[i])
-            if line[1] <= StartTime:
-                result.append(line)
-    result_return = sorted(result,key=lambda result:result[2])
-    return [CarId[0] for CarId in result_return]
-
-#StartTime = 1
-#path = r"C:\Users\just4\Desktop\suancaiyu-master\program"
-#path += r"\answer.txt"    
-#final = CarSorted(path,StartTime)
+#            if line[1] <= StartTime:
+            result.append(line)
+    result_return = sorted(result,key=lambda result:result[1])
+    return [CarId[0] for CarId in result_return], [CarId[1] for CarId in result_return]
